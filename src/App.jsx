518acd79b950b4d43ec37e92c44c019aeaa9175c
SuryaSivalingam <148701777+SuryaSivalingam@users.noreply.github.com>
2025-12-01 08:13:13 +0000

    added Dockerfile and react code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import {add,subtract,multiply,divide} from '@suryasivalingam/gh-lessontest'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <input id="num1" />
+        <input id="num2" />
+        <button onClick={ ()=> alert(multiply(document.getElementById('num1').value,document.getElementById('num2').value))}>Multiply</button>
+        <button onClick={ ()=> alert(divide(document.getElementById('num1').value,document.getElementById('num2').value))}>Divide</button>
+        <button onClick={ ()=> alert(add(document.getElementById('num1').value,document.getElementById('num2').value))}>Add</button>
+        <button onClick={ ()=> alert(subtract(document.getElementById('num1').value,document.getElementById('num2').value))}>Subtract</button>
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
